70b31c2542a27856847bcf4f7d3b6b0e47bfb085
Aniket Pokale <your-email@gmail.com>
2026-04-21 19:25:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// const dotenv = require('dotenv');
+// dotenv.config();
+const express = require('express');
+const dotenv = require('dotenv');
+ dotenv.config();
+const app = express();
+require('./util/db');
+const Data = require('./util/config');  
+
+const  cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+app.post('/data',async(req,res)=>{
+let data= new Data(req.body);  
+let result= await data.save();
+res.send(result);
+})
+
+app.listen(4000,()=>{
+    console.log('Server is running on port 4000');
+})
---
+++ util/config.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const dataschema= new mongoose.Schema({
+    name:String,
+    email:String,
+    subject: String,
+    message: String
+})
+
+module.exports=mongoose.model('Data',dataschema);
